daa4a5fee382ebdda0078847a77dad482ac8b289
Richard Vankoningsveld <richyvk@gmail.com>
2017-09-30 09:48:07 +1000

    Finish challenge two tests

--- tests.py
+++ tests.py
@@ -17,17 +17,80 @@ class TestIntermediateAlgs(unittest.TestCase):
 
     def test_diff_array(self):
         """Test diff two arrays function."""
-        self.assertTrue(type(fai.diff_array([1, 2, 3, 5], [1, 2, 3, 4, 5])) == list)
+        diff_array_type = type(fai.diff_array([1, 2, 3, 5], [1, 2, 3, 4, 5]))
+        self.assertTrue(diff_array_type == list)
+
         self.assertEqual(["pink wool"],
-                         fai.diff_array(["diorite", "andesite", "grass",
-                                         "dirt", "pink wool", "dead shrub"],
-                                        ["diorite", "andesite", "grass",
-                                         "dirt", "dead shrub"]))
-        self.assertEqual(sorted(["diorite", "pink wool"]),
-                         sorted(fai.diff_array(["andesite", "grass", "dirt",
-                                                "pink wool", "dead shrub"],
-                                               ["diorite", "andesite", "grass",
-                                                "dirt", "dead shrub"])))
+                         fai.diff_array(["diorite",
+                                         "andesite",
+                                         "grass",
+                                         "dirt",
+                                         "pink wool",
+                                         "dead shrub"],
+                                        ["diorite",
+                                         "andesite",
+                                         "grass",
+                                         "dirt",
+                                         "dead shrub"]))
+
+        self.assertEqual(sorted(["diorite",
+                                 "pink wool"]),
+                         sorted(fai.diff_array(["andesite",
+                                                "grass",
+                                                "dirt",
+                                                "pink wool",
+                                                "dead shrub"],
+                                               ["diorite",
+                                                "andesite",
+                                                "grass",
+                                                "dirt",
+                                                "dead shrub"])))
+
+        self.assertEqual([],
+                         fai.diff_array(["andesite",
+                                         "grass",
+                                         "dirt",
+                                         "dead shrub"],
+                                        ["andesite",
+                                         "grass",
+                                         "dirt",
+                                         "dead shrub"]))
+
+        self.assertEqual([4],
+                         fai.diff_array([1, 2, 3, 5],
+                                        [1, 2, 3, 4, 5]))
+
+        self.assertEqual(sorted(["piglet",
+                                 4]),
+                         sorted(fai.diff_array([1,
+                                                "calf",
+                                                3,
+                                                "piglet"],
+                                               [1,
+                                                "calf",
+                                                3,
+                                                4])))
+
+        self.assertEqual(sorted(["snuffleupagus",
+                                 "cookie monster",
+                                 "elmo"]),
+                         sorted(fai.diff_array([],
+                                               ["snuffleupagus",
+                                                "cookie monster",
+                                                "elmo"])))
+
+        self.assertEqual(sorted([1,
+                                 "calf",
+                                 3,
+                                 "piglet",
+                                 7,
+                                 "filly"]),
+                         sorted(fai.diff_array([1,
+                                                "calf",
+                                                3,
+                                                "piglet"],
+                                               [7,
+                                                "filly"])))
 
 
 if __name__ == '__main__':
